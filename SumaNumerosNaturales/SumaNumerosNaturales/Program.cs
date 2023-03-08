e5f6d7d1a77dcb120c837c4e94dcafb13ece4c52
Andy Morales <andyemmejia@gmail.com>
2023-03-08 16:11:21 -0500

    Ejercicio Suma de los números naturales.

---
+++ SumaNumerosNaturales/SumaNumerosNaturales/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SumaNumerosNaturales
+{
+    public class SumaNumerosNaturales
+    {
+        static void Main(string[] args)
+        {
+            int num, suma, cont;
+
+            cont = 0;
+            suma = 0;
+
+            Console.WriteLine("***************************************************************************************");
+            Console.WriteLine("* ALGORITMO PARA SUMAR LOS NÚMEROS IGUALES E INFERIORES A UN NÚMERO NATURAL INGRESADO *");
+            Console.WriteLine("***************************************************************************************");
+
+            Console.WriteLine("Por favor ingrese un número natural: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if( num < 0 ) 
+            {
+                Console.WriteLine("--El número ingresado no es un número natural--");
+            }
+            else 
+            { 
+                while( cont <= num ) 
+                {
+                    suma = suma + cont;
+                    cont++;
+                }
+                Console.WriteLine($"La suma de los números naturales del número ingresado es: {suma}");
+            }
+        }
+    }
+}
