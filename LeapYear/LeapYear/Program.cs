c0e34fa0d1f794bce2f906f26e6fe6713fad7426
Andy Morales <andyemmejia@gmail.com>
2023-03-08 16:57:38 -0500

    Ejercicio LeapYear

---
+++ LeapYear/LeapYear/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeapYear
+{
+    public class LeapYear
+    {
+        static void Main(string[] args)
+        {   
+            int year, sw=1;
+            int multcuatro=4, multcien=100, multcuatrocient=400;
+
+         
+
+            while (sw == 1) 
+            {
+                Console.WriteLine("****************");
+                Console.WriteLine("*Ingrese el año*");
+                Console.WriteLine("****************");
+                year = Convert.ToInt32(Console.ReadLine());
+
+                if ((year % multcuatrocient == 0) | (year % multcuatro == 0) & (year % multcien != 0))
+                {
+                    Console.WriteLine($"El año {year} es bisiesto");
+                }
+                else 
+                {
+                    Console.WriteLine($"El año {year} no es bisiesto");
+                }
+                Console.WriteLine("Si desea  ingresar otro año, digite 1 ó 0 para finalizar!");
+                sw = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
