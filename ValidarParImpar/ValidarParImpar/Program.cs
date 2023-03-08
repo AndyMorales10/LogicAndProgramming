70123b9f6f25290db9f2dd8aa85968735a947958
Andy Morales <andyemmejia@gmail.com>
2023-03-08 13:53:56 -0500

    Actualización.

--- ValidarParImpar/ValidarParImpar/Program.cs
+++ ValidarParImpar/ValidarParImpar/Program.cs
@@ -2,11 +2,33 @@
 
 namespace ValidarParImpar
 {
-    internal class Program
+    public class ValidarParImpar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1, num2;
+
+            Console.WriteLine("Ingrese el primer número");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("El primer número es par: " + num1);
+            }
+            else
+            {
+                Console.WriteLine("El primer número es impar: " + num1);
+            }
+            if (num2 % 2 == 0)
+            {
+                Console.WriteLine("El segundo número es par: " + num2);
+            }
+            else
+            {
+                Console.WriteLine("El segundo número es impar: " + num2);
+            }
         }
     }
 }
