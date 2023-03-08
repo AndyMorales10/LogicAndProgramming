a951e71dbd8c593792d7b51b4a0628b5aa9ba191
Andy Morales <andyemmejia@gmail.com>
2023-03-08 13:50:48 -0500

    Solución ejercicio Validar ParImpar.

---
+++ ValidarParImpar/ValidarParImpar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValidarParImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
