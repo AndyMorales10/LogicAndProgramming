6f293da7bceff2a4a6796b5e1296042fe3583e1d
Andy Morales <andyemmejia@gmail.com>
2023-03-09 15:14:24 -0500

    Ejercicio contador números positivos.

---
+++ CicloMientrasNumPositivoNumNegativo/CicloMientrasNumPositivoNumNegativo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+
+namespace CicloMientrasNumPositivoNumNegativo
+{
+    public class CicloMientrasNumPositivoNumNegativo
+    {
+        static void Main(string[] args)
+        {
+            int numIngresado, sw=1, contador=0;
+            string message, header1, header2;
+
+            message = ("Por favor ingrese su número:");
+            header1 = ("************************************************************");
+            header2 = ("* ALGORITMO PARA CAPTURAR LA CANTIDAD DE NÚMEROS POSITIVOS *");
+
+            while (sw==1) 
+            {
+                Console.WriteLine(header1);
+                Console.WriteLine(header2);
+                Console.WriteLine(header1);
+                Console.WriteLine("");
+                Console.WriteLine(message);
+                numIngresado = Convert.ToInt32(Console.ReadLine());
+
+                if (numIngresado >= 0)
+                {
+                    contador++;
+                }
+                else
+                {
+                    sw = 0;
+                }
+                Console.Clear();
+
+            }
+            Console.WriteLine(header1);
+            Console.WriteLine(header2);
+            Console.WriteLine(header1);
+            Console.WriteLine("");
+            Console.WriteLine($"La cantidad de números positivos ingresados fue de: {contador}");
+        }
+    }
+}
